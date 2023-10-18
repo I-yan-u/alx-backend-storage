9970f44ddf508eebe302719502c06a66954704d6
Iyanu Ajimobi <iyanuajimbi12@gmail.com>
2023-10-18 23:00:25 +0100

    writing strings to redis

---
+++ 0x02-redis_basic/exercise.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python3
+"""
+Redis caching implementation
+"""
+import redis
+import uuid
+from typing import Union
+
+class Cache:
+    """ Cache implementation with redis """
+    def __init__(self):
+        """ Init method """
+        self._redis = redis.Redis()
+        self.flushdb = self._redis.flushdb()
+
+    def store(self, data: Union[str, int, float, bytes]) -> str:
+        """ cache storage method """
+        key = str(uuid.uuid4())
+        self._redis.mset({key: data})
+        return key
